fe85473d891f1764287a9cf6d0a288d68d5365a0
Sora0432 <raphael.kiecken.etu@univ-lille.fr>
2023-01-30 09:08:56 +0100

    Up

--- src/sae2/UseTestSae.java
+++ src/sae2/UseTestSae.java
@@ -83,49 +83,4 @@ class UseTestSae {
             }
         }
     }
-
-    public static void main(String[] args) {
-        TestSae t = new TestSae();
-        System.out.println(t);
-        TestSae t2 = new TestSae();
-        System.out.println("Sort on first name ascending:");
-        sortOn(t2, true, 0);
-        System.out.println(t2);
-        TestSae t3 = new TestSae();
-        System.out.println("Sort on first name descending:");
-        sortOn(t3, false, 0);
-        System.out.println(t3);
-        TestSae t4 = new TestSae();
-        System.out.println("Sort on last name ascending:");
-        sortOn(t4, true, 1);
-        System.out.println(t4);
-        TestSae t5 = new TestSae();
-        System.out.println("Sort on last name descending:");
-        sortOn(t5, false, 1);
-        System.out.println(t5);
-        TestSae t6 = new TestSae();
-        System.out.println("Sort on grade ascending:");
-        sortOn(t6, true, 2);
-        System.out.println(t6);
-        TestSae t7 = new TestSae();
-        System.out.println("Sort on grade descending:");
-        sortOn(t7, false, 2);
-        System.out.println(t7);
-        TestSae t8 = new TestSae();
-        System.out.println("Sort on year ascending:");
-        sortOn(t8, true, 3);
-        System.out.println(t8);
-        TestSae t9 = new TestSae();
-        System.out.println("Sort on year descending:");
-        sortOn(t9, false, 3);
-        System.out.println(t9);
-        TestSae t10 = new TestSae();
-        System.out.println("Sort on year descending, grade descending and first name ascending:");
-        sortOn(t10, false, 3);
-        sortOn(t10, false, 2);
-        sortOn(t10, true, 0);
-        System.out.println(t10);
-
-    }
-
 }
