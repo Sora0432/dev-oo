5839258643a09c5441ec885f356cfc529783bfcf
Sora0432 <raphael.kiecken.etu@univ-lille.fr>
2023-02-01 22:40:15 +0100

    Finished tp02

--- src/tp02/NDicePlayerGame.java
+++ src/tp02/NDicePlayerGame.java
@@ -20,13 +20,12 @@ class NDicePlayerGame {
 
     DicePlayer[] winner() {
         ArrayList<DicePlayer> winnersTemp = new ArrayList<DicePlayer>();
-        int minNbRoll = this.playerList[0].nbDiceRolls;
-        winnersTemp.add(this.playerList[0]);
-        for (int i = 1; i < this.playerList.length; i++) {
-            if (this.playerList[i].nbDiceRolls < minNbRoll) {
+        int minNbRoll = -1;
+        for (int i = 0; i < this.playerList.length; i++) {
+            if (this.playerList[i].nbDiceRolls >= this.OBJECTIVE && this.playerList[i].nbDiceRolls < minNbRoll) {
                 winnersTemp.clear();
                 winnersTemp.add(this.playerList[i]);
-            } else {
+            } else if (this.playerList[i].nbDiceRolls >= this.OBJECTIVE) {
                 winnersTemp.add(this.playerList[i]);
             }
         }
@@ -35,6 +34,15 @@ class NDicePlayerGame {
             winners[i] = winnersTemp.get(i);
         }
         return winners;
+    }
+
+    boolean isFinished() {
+        for (int i = 0; i < this.playerList.length; i++) {
+            if (this.playerList[i].totalValue >= this.OBJECTIVE) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -47,43 +55,36 @@ class NDicePlayerGame {
         Dice dice = new Dice(scanner.nextInt());
         System.out.println('\n');
 
-        System.out.println("Objective: 20 points.\n");
+        System.out.println("Objective: " + game.OBJECTIVE + " points.\n");
 
         String userInput;
 
-        while (game.playerOne.totalValue < 20 && game.playerTwo.totalValue < 20) {
-            System.out.println(game.playerOne.name + ", do you want to roll the dice? (yes/no)");
+        while (!game.isFinished()) {
+            for (int i = 0; i < game.playerList.length; i++) {
+                System.out.println(game.playerList[i].name + ", do you want to roll the dice? (yes/no)");
 
-            do {
-                userInput = scanner.nextLine().toLowerCase();
-            } while (!userInput.equals("yes") && !userInput.equals("no"));
+                do {
+                    userInput = scanner.nextLine().toLowerCase();
+                } while (!userInput.equals("yes") && !userInput.equals("no"));
 
-            if (userInput.equals("yes")) {
-                game.playerOne.play(dice);
-                System.out.println("Here is your current stats: " + game.playerOne);
-                System.out.println('\n');
-            } else if (userInput.equals("no")) {
-                System.out.println("Merci d'avoir jouer.");
-                break;
-            }
-
-            System.out.println(game.playerTwo.name + ", do you want to roll the dice? (yes/no)");
-
-            do {
-                userInput = scanner.nextLine().toLowerCase();
-            } while (!userInput.equals("yes") && !userInput.equals("no"));
-
-            if (userInput.equals("yes")) {
-                game.playerTwo.play(dice);
-                System.out.println("Here is your current stats: " + game.playerTwo);
-            } else if (userInput.equals("no")) {
-                System.out.println("Merci d'avoir jouer.");
-                break;
+                if (userInput.equals("yes")) {
+                    game.playerList[i].play(dice);
+                    System.out.println("Here is your current stats: " + game.playerList[i]);
+                    System.out.println('\n');
+                } else if (userInput.equals("no")) {
+                    System.out.println("Merci d'avoir jouer.");
+                    break;
+                }
             }
         }
 
-        if (game.playerOne.totalValue >= 20 || game.playerTwo.totalValue >= 20) {
-            System.out.println("Le gagnant est " + game.winner());
+        if (game.isFinished()) {
+            System.out.println("The game is finished.");
+            System.out.println("The winner(s) is/are: ");
+            DicePlayer[] winners = game.winner();
+            for (int i = 0; i < winners.length; i++) {
+                System.out.println(winners[i].name);
+            }
         }
 
         scanner.close();
